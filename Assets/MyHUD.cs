40edc720cf753848dbc813a5d79eaf3e10fc7699
Trimadzo <trimadzo@gmail.com>
2018-05-17 13:41:04 +0200

    Network Manager 01

---
+++ Assets/MyHUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyHUD : MonoBehaviour
+{
+
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = GetComponent<NetworkManager>();
+    }
+
+    public void MyStartHost()
+    {
+        Debug.Log("Starting Host at " + Time.timeSinceLevelLoad);
+        networkManager.StartHost();
+    }
+
+    void OnStartHost()
+    {
+        Debug.Log("Host started at " + Time.timeSinceLevelLoad);
+    }
+
+}
