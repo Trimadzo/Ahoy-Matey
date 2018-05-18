4c0e7e0774bb009df2dd06d819ebaee75468f07a
Trimadzo <trimadzo@gmail.com>
2018-05-18 14:32:12 +0200

    Player local authority

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    private Vector3 inputValue;
+    public float speedScaling = 0.5f;
+	
+	void Update () {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal") * speedScaling;
+        inputValue.y = 0f;
+        inputValue.z = CrossPlatformInputManager.GetAxis("Vertical") * speedScaling;
+
+        transform.Translate(inputValue);
+    }
+}
